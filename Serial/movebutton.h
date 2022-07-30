c9c89c12802d4d6bc3b85326f5b2f0069cdaaf24
TotoroLXZ <1142206684@qq.com>
2022-07-30 11:26:20 +0800

    分支A修改

--- Serial/movebutton.h
+++ Serial/movebutton.h
@@ -24,7 +24,7 @@ public:
     /*........当前按钮的命令............增加动态命令数据.*/
     QList<QByteArray> BtnDataLists;//动态创建按钮的列表
     QByteArray BtnDatas;
-    /*........配置当前按钮的命令.............*/
+    /*........配置当前按钮的命令.....  后期要删掉  做动态改增加命令和删除 修改命令........*/
     void setBtnDatas(QByteArray btndatas);
     /*........获取当前按钮的命令.............*/
     QByteArray getBtnDatas();
